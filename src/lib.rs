6ae1bb28f4921fdd892c9787a74af89f4a614224
p4ken <commit@p4ken.skr.jp>
2024-05-13 05:41:29 +0900

    API Drafts

--- src/lib.rs
+++
@@ -1,5 +0,0 @@
-pub struct Degree {
-    pub lat: f64,
-    pub lon: f64,
-}
-// pub fn from_degree() -> LatLon<f64> {}
--- tests/draft.rs
+++ tests/draft.rs
@@ -1,11 +1,18 @@
-use datumjp;
-use latlonjp::{
-    Datum, Degree, Jgd2011, LatLon, TokyoDatum, Transform, Unit, TOKYO, TOKYO_TO_JGD2000,
+mod datumjp {
+    pub fn tokyo<T>(_: T) -> TokyoDatum {}
+    pub fn tokyo_to_jgd2000() -> JGD2000 {}
+    pub fn tky2jgd() -> Transform {}
+}
+
+use datumjp::{
+    Datum, Degree, Jgd2011, LatLon, TokyoDatum, Transform, Unit, JGD2011, TOKYO, TOKYO_TO_JGD2000,
+    XY,
 };
 
 fn api_elements() {
     let bl = geo_types::Point::new(2.0, 1.0);
     let bl = LatLon(1.0, 2.0);
+    let xy = XY(1.0, 2.0);
 
     // let degree = latlonjp::degree(1.0, 2.0); // ambiguous lat/lon
     // let degree = LatLon {
@@ -17,9 +24,10 @@ fn api_elements() {
     let degree = LatLon::with_degree(1.0, 2.0);
     let degree = Transform::degree(bl);
 
-    // let tokyo = degree.as_tokyo(); // wrong "as"
+    let tokyo = degree.as_tokyo();
+    let tokyo = TokyoDatum(degree);
     let tokyo = TokyoDatum::new(degree);
-    // let tokyo = TokyoDatum { lat: 1.0, lon: 2.0 }; // ambiguous unit
+    let tokyo = TokyoDatum { lat: 1.0, lon: 2.0 };
     // let tokyo = LatLon {
     //     lat: 1.0,
     //     lon: 1.0,
@@ -33,11 +41,11 @@ fn api_elements() {
     };
     let tokyo = TokyoDatum::with_degree(bl);
     let tokyo = Transform::from_tokyo(degree);
-    let tokyo = latlonjp::tokyo(degree);
+    let tokyo = datumjp::tokyo(degree);
 
     let transform = Transform::from_tokyo().to_jgd2000().to_jgd2011();
     let transform = TOKYO.to_jgd2000().to_jgd2011();
-    let transform = latlonjp::tokyo_to_jgd2000().to_jgd2011();
+    let transform = datumjp::tokyo_to_jgd2000().to_jgd2011();
     let transform = TOKYO_TO_JGD2000.to_jgd2011();
     let transform = datumjp::tky2jgd().tohokutaiheiyouoki().inverse();
 
@@ -45,7 +53,9 @@ fn api_elements() {
     let jgd2011 = tokyo.to_jgd2011();
     // let jgd2011 = bl.transform(Datum::Tokyo, Datum::Jgd2011); // combinatorial explosion
     // let jgd2011 = bl.tokyo_to_jgd2011(); // combinatorial explosion
+    let jgd2011 = degree.transform(&transform);
 
+    let JGD2011 { lat, lon } = jgd2011;
     let Degree { lat, lon } = jgd2011.degree();
     let LatLon(lat, lon) = jgd2011.degree();
 
@@ -55,27 +65,30 @@ fn api_elements() {
 }
 
 fn api_usage() {
+    // let LatLon(lat, lon) = LatLon(1.0, 2.0)
+    //     .as_tokyo()
+    //     .to_jgd2000()
+    //     .to_jgd2011()
+    //     .latlon();
+
     let LatLon(lat, lon) = TokyoDatum::with_degree(LatLon(1.0, 2.0))
         .to_jgd2000()
         .to_jgd2011()
         .degree();
 
-    let Degree { lat, lon } = Transform::from_tokyo(Degree { lat: 1.0, lon: 2.0 }) // Transform<Tokyo<Degree>>
-        .to_jgd2000() // Transform<Jgd2000<Degree>>
-        .to_jgd2011() // Transform<Jgd2011<Degree>>
-        .degree();
+    let JGD2011 { lat, lon } = TokyoDatum { lat: 1.0, lon: 2.0 }.to_jgd2000().to_jgd2011();
 
-    let LatLon(lat, lon) = Transform::degree(LatLon(1.0, 2.0))
-        .from_tokyo()
+    let LatLon(lat, lon) = TokyoDatum(LatLon(1.0, 2.0)).to_jgd2000().to_jgd2011().0;
+
+    let LatLon(lat, lon) = Transform::from_tokyo(LatLon(1.0, 2.0))
         .to_jgd2000()
         .to_jgd2011()
-        .degree(); // robust
+        .transform();
 
-    // let p = geo_types::Point::new(2.0, 1.0);
-    // let _ = TokyoDatum::with_degree(p)
-    //     .to_jgd2000()
-    //     .to_jgd2011()
-    //     .degree()
-    //     .into();
-    // let _ = Transform::tokyo(p).to_jgd2000().to_jgd2011();
+    let LatLon(lat, lon) = Transform::from_tokyo()
+        .to_jgd2000()
+        .to_jgd2011()
+        .transform(LatLon(1.0, 2.0));
+
+    let p = geo_types::Point::new(2.0, 1.0);
 }
