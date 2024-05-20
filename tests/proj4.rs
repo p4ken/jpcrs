4e2151c2cf7e683a769c10ef83d53de504f0e33a
p4ken <commit@p4ken.skr.jp>
2024-05-20 18:14:00 +0900

    Revert "WIP proj4rs"
    This reverts commit a999e01268a7f3e2563ddcca7f7389ab1a549957.

--- tests/proj4.rs
+++ tests/proj4.rs
@@ -1,15 +1,2 @@
-use proj4rs::{
-    nadgrids::{catalog, Catalog},
-    Proj,
-};
-
 #[test]
-fn proj4() -> anyhow::Result<()> {
-    let grid = ????;
-    catalog::add_grid(
-        "/Users/p4/Downloads/JapanGridShift-master/gsb_files/TKY2JGD.gsb".into(),
-        grid,
-    )?;
-    Proj::from_proj_string("+proj=latlong +ellps=bessel +towgs84=-146.414,507.337,680.507 +nadgrids=/Users/p4/Downloads/JapanGridShift-master/gsb_files/TKY2JGD.gsb,null +to +proj=latlong +ellps=GRS80 +towgs84=0,0,0").unwrap();
-    ok(())
-}
+fn proj4() {}
