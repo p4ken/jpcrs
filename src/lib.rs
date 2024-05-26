7f67194c5f6aab4304a328d5445889e548aca87f
p4ken <commit@p4ken.skr.jp>
2024-05-26 21:33:11 +0900

    FIx build error

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@ mod par;
 
 pub use coord::{Degree, LatLon, XY};
 pub use crs::{Tokyo97, TokyoDatum, JGD2000, JGD2011};
-pub use grid::{Grid, TKY2JGD};
+pub use grid::Grid;
+#[cfg(feature = "tky2jgd")]
+pub use grid::TKY2JGD;
 
 pub fn from_tokyo(p: impl Into<XY>) -> TokyoDatum {
     todo!()
